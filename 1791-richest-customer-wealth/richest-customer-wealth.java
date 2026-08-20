e22c79ca68d307c9362449de7495616440d4d63a
Pallav Sharma <pallavsharma2112@gmail.com>
2026-08-20 10:25:21 +0530

    Time: 1 ms (15.80%) | Memory: 44.7 MB (37.59%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -3,11 +3,11 @@ class Solution {
 
         int r = accounts.length ;
         int c = accounts[0].length ; 
-        int currsum =0 ; 
+        
         int maxsum = 0 ; 
 
         for(int i = 0 ; i < r ; i++){
-            currsum = 0 ; 
+           int currsum = 0 ; 
             for(int j = 0 ; j < c; j++){
                currsum += accounts[i][j];
                maxsum = Math.max(currsum , maxsum);
