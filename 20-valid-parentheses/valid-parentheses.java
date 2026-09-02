510d91db06384d09291523d099f5d8f42afbeae1
Pallav Sharma <pallavsharma2112@gmail.com>
2026-09-02 22:31:22 +0530

    Time: 3 ms (86.19%) | Memory: 42.9 MB (96.68%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -21,10 +21,8 @@ class Solution {
 
         }
 
-        if(!stack.isEmpty()){
-            return false ;
-        }
-         return true ;
+       
+         return stack.isEmpty(); 
         
     }
 }
